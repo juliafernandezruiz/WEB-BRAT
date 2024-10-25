a1b836449906d43aa7ad11c52d9f3acbfc046d5d
arianabaniv <arianabaniv@gmail.com>
2024-10-25 19:45:46 +0200

    Cambioss

--- js/script.js
+++ js/script.js
@@ -11,7 +11,38 @@ var typed = new Typed("#typed-text", {
     typeSpeed: 80,
 });
 
+<<<<<<< Updated upstream
 
 
 
+=======
+ScrollReveal().reveal(".grid-wrapper > div", {
+    delay: 200,
+    distance: "50px",
+    interval: 100,
+    origin: "bottom",
+    scale: 0.9,
+});
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-slide');
+    const track = document.querySelector('.carousel-track');
+    const slideWidth = slides[0].clientWidth;
+
+    track.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+function nextSlide() {
+    const slides = document.querySelectorAll('.carousel-slide');
+    currentIndex = (currentIndex + 1) % slides.length; // Siguiente slide
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    const slides = document.querySelectorAll('.carousel-slide');
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length; // Slide anterior
+    showSlide(currentIndex);
+}
+>>>>>>> Stashed changes
 
